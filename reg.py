84bd04ab6dbb3fc4254af0a5cc88c6e9d733a883
mferiansyahrt <muhammadferiansyahraihan@gmail.com>
2025-03-10 17:53:38 +0700

    change reg.py multiplier

--- reg.py
+++ reg.py
@@ -6,8 +6,8 @@ from sklearn.model_selection import train_test_split
 
 # Generate dummy dataset
 np.random.seed(42)
-X = np.random.rand(100, 1) * 10  # Feature
-y = 2.5 * X + np.random.randn(100, 1) * 2  # Target with some noise
+X = np.random.rand(100, 1) * 5  # Feature
+y = 2.5 * X + np.random.randn(100, 1) * 10  # Target with some noise
 
 # Split the dataset
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
